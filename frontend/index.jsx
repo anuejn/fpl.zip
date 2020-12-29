bdc0c965c59c621ae23596762738792e85a4154c
Jaro Habiger <jarohabiger@googlemail.com>
2020-12-29 14:45:08 +0100

    format

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import "./index.css"
+import "./index.css";
 
 if (module.hot) {
   module.hot.accept();
