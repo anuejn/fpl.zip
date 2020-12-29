cee34ab0a84453e2dfa9834cc68d0cdd1ab07eeb
pajowu <git@ca.pajowu.de>
2020-12-29 14:13:40 +0100

    🎨 Run 'yarnpkg format'

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,12 +1,15 @@
-import React, {useEffect, useState} from "react";
-import useFetch from 'use-http'
+import React, { useEffect, useState } from "react";
+import useFetch from "use-http";
 import useSound from "use-sound";
-
 
 const audioCtx = new AudioContext();
 
 export default function App(props) {
-  const { loading, error, data = [] } = useFetch('https://files.niemo.de/kondome.mp3.json', {}, [])
+  const { loading, error, data = [] } = useFetch(
+    "https://files.niemo.de/kondome.mp3.json",
+    {},
+    []
+  );
 
   const [timeRange, setTimeRange] = useState([0, 3000]);
   useEffect(() => {
@@ -14,40 +17,55 @@ export default function App(props) {
       const selection = document.getSelection();
       const range = selection.getRangeAt(0);
 
-
       if (data) {
-        const tagged_data = data.result.reduce((acc, curr) => {
-          let lastIndex = 0;
-          if (acc.length > 0) {
-            lastIndex = acc[0].endIndex
-          }
+        const tagged_data = data.result
+          .reduce((acc, curr) => {
+            let lastIndex = 0;
+            if (acc.length > 0) {
+              lastIndex = acc[0].endIndex;
+            }
 
-          return [{startIndex: lastIndex, endIndex: lastIndex + curr.word.length + 1, ...curr}, ...acc]
-        }, []).reverse()
+            return [
+              {
+                startIndex: lastIndex,
+                endIndex: lastIndex + curr.word.length + 1,
+                ...curr,
+              },
+              ...acc,
+            ];
+          }, [])
+          .reverse();
 
-        const words = tagged_data.filter(word => word.endIndex - 1 > range.startOffset && word.startIndex < range.endOffset);
-        console.log(words.map(x => x.word))
-        const [start, end] = [Math.min(...words.map(x => x.start)), Math.max(...words.map(x => x.end))]
-        setTimeRange([start * 1000, (end - start) * 1000])
+        const words = tagged_data.filter(
+          (word) =>
+            word.endIndex - 1 > range.startOffset &&
+            word.startIndex < range.endOffset
+        );
+        console.log(words.map((x) => x.word));
+        const [start, end] = [
+          Math.min(...words.map((x) => x.start)),
+          Math.max(...words.map((x) => x.end)),
+        ];
+        setTimeRange([start * 1000, (end - start) * 1000]);
       }
-    }
-  })
+    };
+  });
 
-  const [play, {sound}] = useSound('https://files.niemo.de/kondome.mp3', {
+  const [play, { sound }] = useSound("https://files.niemo.de/kondome.mp3", {
     onload: () => console.info("sound loaded"),
     onend: (a, b, c) => {
-      console.info('Sound ended!', a, b, c);
+      console.info("Sound ended!", a, b, c);
     },
     sprite: {
-      'selection': timeRange,
-    }
+      selection: timeRange,
+    },
   });
 
   if (error) {
-    return 'Error!'
+    return "Error!";
   }
   if (loading) {
-    return 'Loading...'
+    return "Loading...";
   }
 
   return (
@@ -56,9 +74,11 @@ export default function App(props) {
       <button
         onClick={() => {
           sound._sprite.selection = timeRange;
-          play({'id': 'selection'})
+          play({ id: "selection" });
         }}
-      >play</button>
+      >
+        play
+      </button>
     </>
-  )
+  );
 }
